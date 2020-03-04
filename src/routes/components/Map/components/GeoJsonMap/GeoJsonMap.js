2e52df78c32c1257e6272b302d09db3639125204
stefanosKarantin <karantinakis.s@gmal.com>
2020-03-05 01:55:17 +0200

    styling selecting ad hovering polygons

--- src/routes/components/Map/components/GeoJsonMap/GeoJsonMap.js
+++ src/routes/components/Map/components/GeoJsonMap/GeoJsonMap.js
@@ -6,8 +6,11 @@ import { Map, View } from 'ol';
 import { Tile as TileLayer, Vector as LayerVector} from 'ol/layer';
 import { OSM, TileWMS, Vector as SourceVector} from 'ol/source';
 import GeoJSON from 'ol/format/GeoJSON';
-
+import { Style, Fill, Stroke}from 'ol/style'
 import { connectProps } from 'store';
+import Select from 'ol/interaction/Select';
+import { click, pointerMove } from 'ol/events/condition';
+import Collection from 'ol/Collection';
 
 import {
     geodata,
@@ -15,6 +18,37 @@ import {
     toggleLoadingTrue,
     getGeoData
 } from 'modules/component-props';
+
+const highlightStyle = new Style({
+    fill: new Fill({
+        color: 'rgba(255,255,255,0.7)'
+    }),
+    stroke: new Stroke({
+        color: '#046098',
+        width: 3
+    })
+});
+
+const highlightClickedStyle = new Style({
+    fill: new Fill({
+        color: 'rgba(255,255,255,0.7)'
+    }),
+    stroke: new Stroke({
+        color: '#052d45',
+        width: 3
+    })
+});
+
+const rasterValStyle = (feature) => {
+    const colors = ["#E9967A", "#DAA520", "#800080", "#00008B", "#FF0000", "#BC8F8F", "#2F4F4F", "#FFE4C4", "#F4A460"]
+
+    feature.setStyle(new Style({
+        stroke: new Stroke({
+            color: colors[feature.get("rasterVal")],
+            width: 5
+        })
+    }));
+};
 
 const createGeoJsonMap = (center, zoom) => {
     const vectorSource = new SourceVector();
@@ -36,6 +70,93 @@ const createGeoJsonMap = (center, zoom) => {
         })
     });
     document.getElementById('map').data = map
+
+    let selected = null;
+    let clickSelected = null;
+    const status = document.getElementById('status');
+
+    map.on('pointermove', e => {
+        if (selected !== null && selected !== clickSelected) {
+            selected.setStyle(undefined);
+            selected = null;
+        }
+
+        map.forEachFeatureAtPixel(e.pixel, f => {
+            selected = f;
+            selected !== clickSelected && f.setStyle(highlightStyle);
+            return true;
+        });
+
+        if (selected) {
+            status.innerHTML = '&nbsp;Hovering: ' + selected.get('name');
+        } else {
+            status.innerHTML = '&nbsp;';
+        }
+      });
+
+      map.on('click', e => {
+        if (clickSelected !== null) {
+            clickSelected.setStyle(undefined);
+            clickSelected = null;
+        }
+
+        map.forEachFeatureAtPixel(e.pixel, f => {
+            console.log(e)
+            console.log("-------")
+            console.log(f)
+
+            clickSelected = f;
+            f.setStyle(highlightClickedStyle);
+            return true;
+        });
+
+        if (clickSelected) {
+            status.innerHTML = '&nbsp;Hovering: ' + clickSelected.get('name');
+        } else {
+            status.innerHTML = '&nbsp;';
+        }
+      });
+    const select = null; // ref to currently selected interaction
+
+    // select interaction working on "singleclick"
+    const selectSingleClick = new Select();
+
+    // select interaction working on "click"
+    const selectClick = new Select({
+        condition: click
+    });
+
+    // select interaction working on "pointermove"
+    const selectPointerMove = new Select({
+        condition: pointerMove
+    });
+
+    const selectElement = document.getElementById('type');
+
+    const changeInteraction = function() {
+    if (select !== null) {
+        map.removeInteraction(select);
+    }
+    const value = selectElement.value;
+    if (value == 'singleclick') {
+        select = selectSingleClick;
+    } else if (value == 'click') {
+        select = selectClick;
+    } else if (value == 'pointermove') {
+        select = selectPointerMove;
+    } else {
+        select = null;
+    }
+    if (select !== null) {
+        map.addInteraction(select);
+        select.on('select', function(e) {
+        document.getElementById('status').innerHTML = '&nbsp;' +
+            e.target.getFeatures().getLength() +
+            ' selected features (last operation selected ' + e.selected.length +
+            ' and deselected ' + e.deselected.length + ' features)';
+        });
+    }
+    };
 };
 
 const GeoJsonMap = ({
@@ -55,16 +176,17 @@ const GeoJsonMap = ({
     useEffect(() => {
         if (!_.isEmpty(geodata)) {
             const rasterFilters = _.filter(filters, f => f.checked).map(f => f.value);
-            const filteredData = geodata.filter(g => rasterFilters.includes(g[1])).map(p => JSON.parse(p[2]))
+            const filteredData = geodata.filter(g => rasterFilters.includes(g[1])).map(p => ({
+                type: 'Feature',
+                properties: {
+                    rasterVal: p[1]
+                },
+                geometry: JSON.parse(p[2]),
+            }));
+
             const polygons = {
                 type: 'FeatureCollection',
-                features: [{
-                    type: 'Feature',
-                    geometry: {
-                        type: 'GeometryCollection',
-                        geometries: filteredData
-                    }
-                }]
+                features: filteredData
             };
 
             document.getElementById('map').data.getLayers().forEach(layer => {
@@ -72,6 +194,9 @@ const GeoJsonMap = ({
                 if (layer instanceof LayerVector) {
                     layer.getSource().clear()
                     layer.getSource().addFeatures((new GeoJSON()).readFeatures(JSON.stringify(polygons)))
+                    // layer.getSource().addFeatures((new GeoJSON()).readFeatures(polygons))
+                    layer.getSource().getFeatures().forEach(rasterValStyle)
+
                 //     layer.getSource().getFeatures().forEach(f => {
                 //         console.log(f)
                 //     })
@@ -80,7 +205,10 @@ const GeoJsonMap = ({
         }
     }, [geodata, filters]);
     return (
-        <div style={{width: '100%'}} id={'map'} />
+        <div style={{width: '100%'}}>
+            <div style={{width: '100%', height: '100%'}} id={'map'} />
+            <span id={'status'} />
+        </div>
     );
 }
 
